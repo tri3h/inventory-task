763dca7d9fe12f897063887e7a3e71ca50f61bff
unknown <tri3h@ya.ru>
2023-06-23 21:58:23 +0500

    add message panel

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import StatsPanel from "@/components/StatsPanel.vue";
+import MessagePanel from "@/components/MessagePanel.vue";
 </script>
 
 <template>
   <StatsPanel />
+  <MessagePanel />
 </template>
 
 <style scoped></style>
---
+++ src/components/MessagePanel.vue
@@ -0,0 +1,28 @@
+<script setup>
+import InventoryLine from "@/components/UI/InventoryLine.vue";
+</script>
+
+<template>
+  <div class="message">
+    <img class="message__image" src="/icons/close.svg" alt="close" />
+    <InventoryLine :width="699" :height="36" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.message {
+  width: 785px;
+  height: 72px;
+  position: relative;
+  background-color: var(--c-mine-shaft-very-dark);
+  padding: 18px;
+  border-radius: 12px;
+  border: 1px solid var(--c-tundora);
+
+  &__image {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+  }
+}
+</style>
