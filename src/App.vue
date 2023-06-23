0c2a345f047090bdcadb3ffe7600a6630b2d6535
unknown <tri3h@ya.ru>
2023-06-23 22:22:03 +0500

    add inventory panel

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
 import StatsPanel from "@/components/StatsPanel.vue";
 import MessagePanel from "@/components/MessagePanel.vue";
+import InventoryPanel from "@/components/InventoryPanel.vue";
 </script>
 
 <template>
-  <StatsPanel />
-  <MessagePanel />
+  <div class="container">
+    <StatsPanel />
+    <InventoryPanel />
+    <MessagePanel />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 24px;
+}
+</style>
---
+++ src/components/InventoryPanel.vue
@@ -0,0 +1,71 @@
+<script setup></script>
+
+<template>
+  <div class="inventory">
+    <div class="inventory__row">
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+    </div>
+    <div class="inventory__row">
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+    </div>
+    <div class="inventory__row">
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+    </div>
+    <div class="inventory__row">
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+    </div>
+    <div class="inventory__row">
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+      <div class="inventory__cell"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.inventory {
+  width: 525px;
+  height: 500px;
+  border-radius: 12px;
+  border: 1px solid var(--c-tundora);
+  background-color: var(--c-mine-shaft-very-dark);
+
+  &__row {
+    height: 100px;
+    display: flex;
+    border-bottom: 1px solid var(--c-tundora);
+
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+
+  &__cell {
+    width: 105px;
+    height: 100px;
+    border-right: 1px solid var(--c-tundora);
+
+    &:last-child {
+      border-right: none;
+    }
+  }
+}
+</style>
